69c4813870762d500928a547f179cbb07a6a754a
Francois gherabi <gherab_f@localhost.localdomain>
2013-12-04 21:05:00 +0100

    Request code a jour (quelques modif au niveau chat a voir demain)

--- API/RequestCode.hh
+++ API/RequestCode.hh
@@ -23,6 +23,73 @@ namespace request
     static const ID NO_CONTENT =	1004; /**< Request don't have */
     static const ID PARTIAL_CONTENT =	1005; /**< Request have partial content */
     static const ID NO_SLOTS =		1006; /**< No more slots available */
+
+      namespace perso
+      {
+	  static const ID  MISSED_CALLS =	1200;
+	  static const ID  GET_MISSED =		1201;
+	  static const ID  AUTO_ANSWER =	1203;
+      }
+
+      namespace friends
+      {
+	  static const ID UPDATE =	1300;
+      }
+
+      namespace call
+      {
+	  static const ID TIMEOUT =	1400;
+	  static const ID CALL =	1401;
+      }
+
+      namespace chat
+      {
+	  static const ID MESSAGE =	1500;
+      }
+  }
+  namespace client
+  {
+      namespace auth
+      {
+	  static const ID NEW =		0100;
+	  static const ID DELETE =	0101;
+	  static const ID MODIFY =	0102;
+	  static const ID CONNECT =	0103;
+	  static const ID DISCONNECT =	0104;
+      }
+
+      namespace perso
+      {
+	  static const ID PRIVACY_MODE =	0200;
+	  static const ID STATUS =		0201;
+	  static const ID MISSED_CALLS =	0202;
+	  static const ID GET_MISSED =		0203;
+	  static const ID DEL_MISSED =		0204;
+	  static const ID SET_AUTO_ANSWER =	0205;
+	  static const ID UNSET_AUTO_ANSWER =	0206;
+      }
+
+      namespace friends
+      {
+	  static const ID REQUEST =	0300;
+	  static const ID DEL_FRIEND =	0301;
+	  static const ID ACCEPT =	0302;
+	  static const ID REFUSE =	0303;
+	  static const ID LIST =	0304;
+      }
+
+      namespace call
+      {
+	  static const ID CALL =	0400;
+	  static const ID ACCEPT =	0401;
+	  static const ID REFUSE =	0402;
+	  static const ID HANG_UP =	0403;
+      }
+
+      namespace chat
+      {
+	  static const ID MESSAGE =	0500;
+      }
   }
 }
 
